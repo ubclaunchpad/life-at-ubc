1e4a2ff98263efa73c7004763664c18bc07f45a9
Feng Xiong <fengxiong34@gmail.com>
2020-10-17 22:14:16 -0700

    add new branch

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 function App() {
   return (
-    <h1>:)</h1>
+    <div>
+      <h1>:)</h1>
+      <ul>
+        <li>Feng has submitted his PR!</li>
+      </ul>
+    </div>
   );
 }
 
