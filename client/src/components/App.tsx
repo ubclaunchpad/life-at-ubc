ccf9b7e95c5ce355f0faf819f723508c6cbc09d6
Feng Xiong <fengxiong34@gmail.com>
2020-10-24 21:34:39 -0700

    saved

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Home from '../pages/Home';
 import Page1 from '../pages/Page1';
 import Page2 from '../pages/Page2';
@@ -6,14 +6,16 @@ import SlidesController from '../components/SlidesController';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
+  const [a, setA] = useState(true);
   return (
     <div>
+      <div>{`${a}`}</div>
       <BrowserRouter>
-        <Route path='/' exact component={Home}></Route>
-        <Route path='/page1' exact component={Page1}></Route>
-        <Route path='/page2' exact component={Page2}></Route>
+        <Route path='/' exact component={() => <Home />}></Route>
+        <Route path='/page1' exact component={() => <Page1 />}></Route>
+        <Route path='/page2' exact component={() => <Page2 />}></Route>
       </BrowserRouter>
-      <SlidesController></SlidesController>
+      <SlidesController a={a} setA={setA}></SlidesController>
     </div>
   );
 }
--- client/src/components/SlidesController.tsx
+++ client/src/components/SlidesController.tsx
@@ -7,9 +7,22 @@ const StyledSlidesContainer = styled.div`
   }
 `;
 
-function SlidesContainer() {
+type controllerProps = {
+  a: boolean;
+  setA: any;
+};
+
+function SlidesContainer({ a, setA }: controllerProps) {
   return (
     <StyledSlidesContainer>
+      <div
+        onClick={() => {
+          setA(!a);
+          console.log(a);
+        }}
+      >
+        change parent RC state
+      </div>
       <a href='/'>Go to home</a>
       <a href='/page1'>Go to page 1</a>
       <a href='/page2'>Go to page 2</a>
