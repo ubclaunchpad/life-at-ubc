0a3f656bc17216c74b301e4cc6ad09f98b11fd6b
leanna <leanna.resurreccion@gmail.com>
2020-10-27 10:36:08 -0700

    set pretty logs for development only

--- server/utils/logger.ts
+++ server/utils/logger.ts
@@ -1,12 +1,12 @@
 import pino from "pino";
 
+let environment = (process.env.NODE_ENV) || "development";
+
 // Parent logger. If you want to make use of logging in a new file, you MUST import this and create a child logger
 // See index.ts, db.ts, or scraper.ts for examples
 const logger = pino({
     level: process.env.LOG_LEVEL || "info",
-    prettyPrint: {
-        levelFirst: true    // this allows for nice formatted logs, but in production it's recommended to disable this
-    }
+    prettyPrint: (environment === "development")    // makes nice pretty logs but should be disabled for production
 });
 
 export default logger;
