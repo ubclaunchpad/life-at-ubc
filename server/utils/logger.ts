58969559dfeebabf7e65aba9bb79b277a9e90b81
leanna <leanna.resurreccion@gmail.com>
2020-10-26 21:15:24 -0700

    add code + Dockerfile comments

--- server/utils/logger.ts
+++ server/utils/logger.ts
@@ -1,9 +1,11 @@
 import pino from "pino";
 
+// Parent logger. If you want to make use of logging in a new file, you MUST import this and create a child logger
+// See index.ts, db.ts, or scraper.ts for examples
 const logger = pino({
     level: process.env.LOG_LEVEL || "info",
     prettyPrint: {
-        levelFirst: true
+        levelFirst: true    // this allows for nice formatted logs, but in production it's recommended to disable this
     }
 });
 
