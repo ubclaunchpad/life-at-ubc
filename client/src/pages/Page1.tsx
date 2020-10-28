1d605e07bfac773c305d43d2c169d6f2508a112e
Feng Xiong <fengxiong34@gmail.com>
2020-10-27 21:46:33 -0700

    accomplished dispatchStateToProps using useDisptach from react-redux

--- client/src/pages/Page1.tsx
+++ client/src/pages/Page1.tsx
@@ -1,10 +1,29 @@
 import React from "react";
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import { Page1StateProps } from '../reducers/Page1Reducer'
+import { INCREMENT, DECREMENT } from '../actions/Page1Actions';
 
 function Page1() {
-  const number = useSelector<Page1StateProps, Page1StateProps["number"]>((state) => state.number);
-  return <div>{number}</div>;
+  const number = useSelector<Page1StateProps, Page1StateProps['number']>((state) => state.number);
+  const dispatch = useDispatch();
+
+  const handleAddBtnPress = () => {
+    dispatch({type: INCREMENT});
+  }
+
+  const handleMinusBtnPress = () => {
+    dispatch({type: DECREMENT});
+  }
+
+  return (
+  <div>
+    <div>{number}</div>
+    <div>
+      <div style = {{display: 'inline-block', marginRight: 10, cursor: 'pointer'}} onClick = {handleAddBtnPress}>Add</div>
+      <div style = {{display: 'inline-block', cursor: 'pointer'}} onClick = {handleMinusBtnPress}>Minus</div>
+    </div>
+  </div>
+  );
 }
 
 export default Page1;
