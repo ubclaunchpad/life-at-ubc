b1ace4a8b5d2b689ff2cd0aa1f07d3e6f1683552
Feng Xiong <fengxiong34@gmail.com>
2020-10-27 22:06:30 -0700

    passing

--- client/src/pages/Page1.tsx
+++ client/src/pages/Page1.tsx
@@ -4,20 +4,20 @@ import { Page1StateProps } from "../reducers/Page1Reducer";
 import { INCREMENT, DECREMENT } from "../actions/Page1Actions";
 
 function Page1() {
-  const number = useSelector<Page1StateProps, Page1StateProps["number"]>((state) => state.number);
+  const num = useSelector<Page1StateProps, Page1StateProps["number"]>((state) => state.number);
   const dispatch = useDispatch();
 
   const handleAddBtnPress = () => {
     dispatch({type: INCREMENT});
-  }
+  };
 
   const handleMinusBtnPress = () => {
     dispatch({type: DECREMENT});
-  }
+  };
 
   return (
   <div>
-    <div>{number}</div>
+    <div>{num}</div>
       <button style = {{marginRight: 10}} onClick = {handleAddBtnPress}>Add</button>
       <button onClick = {handleMinusBtnPress}>Minus</button>
   </div>
