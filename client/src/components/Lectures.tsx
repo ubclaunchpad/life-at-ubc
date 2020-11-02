e66466aba1a0c0a2b0835019619cf74dc2fb6aae
danielchen-pyc <daniel7711potter@gmail.com>
2020-11-02 18:47:51 +0800

    Added grid component for Lecture, Lab, and Generate. Still need to integrate them to backend

--- client/src/components/Lectures.tsx
+++ client/src/components/Lectures.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { SectionWrapper } from "./Home";
 import Title from "./Title";
+import ScheduleGrid from "./ScheduleGrid"
 
 function Lectures() {
   return (
     <SectionWrapper>
       <Title title="4. Select Lectures to Lock Them"></Title>
+      <ScheduleGrid></ScheduleGrid>
     </SectionWrapper>
   );
 }
