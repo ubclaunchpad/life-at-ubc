9621011db64ddb2d6e95a91b2234c0c12d0c98ae
danielchen-pyc <daniel7711potter@gmail.com>
2020-11-03 17:12:32 +0800

    Switched back to Material UI DataGrid.

--- client/src/components/ScheduleGrid.tsx
+++ client/src/components/ScheduleGrid.tsx
@@ -1,31 +1,71 @@
 import React from "react";
 import { SectionWrapper } from "./Home";
-import { GridComponent, ColumnsDirective, ColumnDirective } from "@syncfusion/ej2-react-grids";
-// import { DataGrid, RowProps } from '@material-ui/data-grid';
-import timeslot from "./timeslot.json"
-import "../grid.css"
+import { DataGrid } from '@material-ui/data-grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const weekday_width = 125;
+const time_width = 90;
+const title_height = 40;
+const row_height = 30;
+
+interface ColDef {
+    /**
+     * The column identifier. It's used to match with [[RowData]] values.
+     */
+    field: string;
+    headerName: string;
+    width: number;
+    sortable: boolean;
+    headerAlign: 'center';
+}
+
+var columns: ColDef[] = [
+    {field: "id", headerName: 'Time', width: time_width, sortable: false, headerAlign: 'center',},
+    {field: "Monday", headerName: 'Mon', width: weekday_width, sortable: false, headerAlign: 'center',},
+    {field: "Tuesday", headerName: 'Tues', width: weekday_width, sortable: false, headerAlign: 'center',},
+    {field: "Wednesday", headerName: 'Wed', width: weekday_width, sortable: false, headerAlign: 'center',},
+    {field: "Thursday", headerName: 'Thur', width: weekday_width, sortable: false, headerAlign: 'center',},
+    {field: "Friday", headerName: 'Fri', width: weekday_width, sortable: false, headerAlign: 'center',},
+];
 
 
-// const { data } = timeslot();
+var rows: any[] = [];
+let startHour = 8, endHour = 17, half = 0;
+for (var hour=startHour; hour<endHour; hour+=0.5) {
+    let minutes = (half%2) === 1 ? ':30': ':00';
+    var time = String(Math.floor(hour)) + minutes;
+    rows.push({
+        id: time,
+    },);
+    half++;
+}
+rows.push({id: "17:00",});
+
 
 function ScheduleGrid() {
-  return (
-    <SectionWrapper>
-        <GridComponent 
-                dataSource={timeslot} 
-                style={{ marginLeft: '30%', marginRight: '30%' }} 
-                rowHeight={25}
-                height='250'
-                width='auto'>
-            <ColumnDirective field='Time' textAlign='Center' width='162.98'></ColumnDirective>
-            <ColumnDirective field='Mon' textAlign='Center' width='162.98'></ColumnDirective>
-            <ColumnDirective field='Tues' textAlign='Center' width='162.98'></ColumnDirective>
-            <ColumnDirective field='Wed' textAlign='Center' width='162.98'></ColumnDirective>
-            <ColumnDirective field='Thu' textAlign='Center' width='162.98'></ColumnDirective>
-            <ColumnDirective field='Fri' textAlign='Center' width='162.98'></ColumnDirective>
-        </GridComponent>
-    </SectionWrapper>
-  );
+    const useStyles = makeStyles(theme => ({
+        root: {
+            marginLeft: 355,
+            marginRight: 355,
+            textAlign: 'center',
+        }
+    }))
+    const classes = useStyles();
+  
+    return (
+        <SectionWrapper>
+            <div style={{ height: 300, width: '50%' }}>
+                <DataGrid 
+                    className={classes.root} 
+                    rows={rows} 
+                    columns={columns} 
+                    rowHeight={row_height} 
+                    headerHeight={title_height}
+                    disableExtendRowFullWidth={false}
+                    hideFooter={true}/>
+            </div>
+        </SectionWrapper>
+    );
 }
 
 export default ScheduleGrid;
