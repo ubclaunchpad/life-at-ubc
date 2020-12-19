2c4001f25f8bf7e18abba2eea3ff0fea7a086455
jacksonyuanjx <jacksonyuanjx@hotmail.com>
2020-12-18 16:14:31 -0800

    .

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -11,7 +11,8 @@ class Database {
     constructor() {
         // If on docker pool should take config, if on local database pool should take localconfig
         this.pool = new Pool((process.env.DB_LOCAL) ? localconfig : config);
-        log.info("====", (process.env.DB_LOCAL), config);
+        // eslint-disable-next-line no-console
+        console.info("====", (process.env.DB_LOCAL), config);
         this.pool.on("error", (err, client) => {
             log.error("Unexpected error on idle PostgreSQL client.", err);
             process.exit(-1);
