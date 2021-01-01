2821b88f806131163f4667de53fbd4c41c7f6bc5
Feng Xiong <fengxiong34@gmail.com>
2021-01-01 09:09:38 -0800

    constants added

---
+++ client/src/util/constants.ts
@@ -0,0 +1,7 @@
+export const MESSAGE = {
+  EMPTY_INPUT: "Empty input",
+  INVALID_FORMAT: "Invalid format",
+  COURSE_NOT_EXIST: "Course does not exist",
+  COURSE_ADD_SUCC: "Course added successfully",
+  COURSE_ALREADY_ADDED: "This course has been added already",
+};
