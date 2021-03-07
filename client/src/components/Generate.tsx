3e01986fc30613ce67ca8821dea9c21a43adad92
John Aguilar <42220961+johnagl@users.noreply.github.com>
2021-03-07 11:24:26 -0800

    Create share link when clicking save button on Generate page

--- client/src/components/Generate.tsx
+++ client/src/components/Generate.tsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Section from "./Section";
 import Title from "./Title";
 import ScheduleGrid from "./ScheduleGrid";
+import Button from "@material-ui/core/Button";
 
-function Generate() {
+import { RootState } from "../reducers/index";
+import { connect } from "react-redux";
+
+interface GenerateProps {
+  selectedSchedule?: any;
+}
+
+function Generate({ selectedSchedule }: GenerateProps) {
+  const [shareLink, setShareLink] = useState("");
+  const handleSave = () => {
+    setShareLink(`sharelink/${JSON.stringify(selectedSchedule)}`);
+  };
   return (
     <Section>
       <Title title="Your Individualized Schedule"></Title>
       <ScheduleGrid></ScheduleGrid>
+      <Button variant="contained" color="secondary" onClick={handleSave}>Save</Button>
+      <p>{shareLink}</p>
     </Section>
   );
 }
 
-export default Generate;
+const mapStateToProps = (state: RootState) => {
+  return {
+    selectedSchedule: state.HomeReducer.selectedSchedule,
+  };
+};
+
+export default connect(mapStateToProps)(Generate);
+
