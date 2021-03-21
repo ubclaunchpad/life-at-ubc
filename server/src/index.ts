25db2a1ad588ead51c567cccf7604d3d7c1f4364
John Aguilar <42220961+johnagl@users.noreply.github.com>
2021-03-20 21:53:38 -0700

    Add Access-Control-Allow-Methods

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,6 +21,7 @@ app.use(express.urlencoded({extended: false}));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "*");
     next();
 });
 app.use(expressLogger);
